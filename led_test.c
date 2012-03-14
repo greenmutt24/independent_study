2785878a459264f8b8e95bc10345fc39ed6c1ad2
Anthony Webb <ajwebb01@gmail.com>
2012-03-13 20:17:07 -0400

    led brightness connected to distance

--- led_test.c
+++ led_test.c
@@ -17,18 +17,25 @@ int pwm(uint8_t percent){
 };
 
 int main(void){
-    
-    //turning port b and c into all outputs
-    DDRB = 0xff;
-    DDRC = 0xff;
+/*Init****************************************************/
+    //enabling needed outputs on port b and c
+    DDRB |= (1<<PB3);//oc9a
+    DDRC |= (1<<PC0);
 
-    //init of pwm
+    //init of pwm on oc0a
     TCCR0A|=(1<<WGM00)|(1<<WGM01)|(1<<COM0A1);
     TCCR0B|=(1<<CS00);
-      
+    
+    //adc set up
+    ADCSRA |= (1<<ADATE)|(1<<ADEN)|(1<<ADSC);
+    ADMUX |= (1<<ADLAR);
+    
+/*End Init************************************************/
     uint8_t brightness = 0;
     while(1){
-        
+        OCR0A = ADCH;
+
+        /*ROTATING BRIGHTNESS
         OCR0A = pwm(brightness++);
         brightness++;
         if(brightness > 100){
@@ -39,13 +46,16 @@ int main(void){
         }
         
         _delay_ms(2000);
+        */
 
-        //PORTB |= (1<<PB0);
-        //_delay_ms(1000);
-        //PORTB = 0x00;
-        //PORTC |=(1<<PC0);
-        //_delay_ms(1000);
-        //PORTC = 0x00;
+
+        /*SWITHING LIGHTS
+        PORTB |= (1<<PB0);
+        _delay_ms(1000);
+        PORTB = 0x00;
+        PORTC |=(1<<PC0);
+        _delay_ms(1000);
+        PORTC = 0x00;*/
     }
     return 0;
 }
