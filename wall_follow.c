832af36ba74ef0dfa3e631c20c2b255c97ec8b18
Anthony Webb <ajwebb01@gmail.com>
2012-03-19 11:33:31 -0400

    wall follow works but not well

--- wall_follow.c
+++ wall_follow.c
@@ -12,7 +12,7 @@
 
 //input the percent you want returns the 8 bit value of
 //given percent
-int pwm(uint8_t percent){
+int percent_to_8bit(uint8_t percent){
     uint8_t duty;
     duty = percent * 2.55;
     return duty;
@@ -42,36 +42,57 @@ PD7 - PUSH BUTTON
 */
 /*Init****************************************************/
     DDRB |= (1<<PB0)|(1<<PB3);
+    PORTB |= (1<<PB0);
     
-    DDRD |= (1<<PD0)|(1<<PD1)|(1<<PD2)|(1<<PD3)|(1<<PD5);
-    PORTD |= (1<<PD7);//ENABLING PULL UP    
+    DDRD |= (1<<PD0)|(1<<PD1)|(1<<PD2)|(1<<PD3)|(1<<PD7);
+    PORTD |= (1<<PD6);//ENABLING PULL UP    
     //init of pwm on OC0A
     TCCR0A|=(1<<WGM00)|(1<<WGM01)|(1<<COM0A1);
     TCCR0B|=(1<<CS00);
     
     //init of pwm on OC1A
-    TCCR1A|=(1<<WGM00)|(1<<WGM01)|(1<<COM0A1);
-    TCCR1B|=(1<<CS00);
+    TCCR2A|=(1<<WGM20)|(1<<WGM21)|(1<<COM2A1);
+    TCCR2B|=(1<<CS20);
     
     //adc set up
     ADCSRA |= (1<<ADATE)|(1<<ADEN)|(1<<ADSC);
-    ADMUX |= (1<<ADLAR);
+    ADMUX |= (1<<ADLAR)|(1<<REFS1)|(1<<REFS0);
     
 /*setup**************************************************/
     int base;
     int i =1;
     PORTD |= (1<<PD1);
     while(i){
-        if(!(PIND & (1<<PD7))){
+        if(!(PIND & (1<<PD6))){
             base = ADCH;
             PORTD &= ~(1<<PD1);
             PORTD |= (1<<PD0);
-            _delay_s(2);
+            _delay_s(5);
             i = 0;
         }
     }
 
+    int prev_left = 50;
+    int prev_right = 50;
+    int band = 15;//percent
     while(1){
-        _delay_ms(1000);
+        PORTD &= ~((1<<PD1)|(1<<PD2)|(1<<PD3));
+        
+        if (ADCH > (base + band)){
+            PORTD |= (1<<PD3);
+            OCR0A = percent_to_8bit(30);
+            OCR2A = percent_to_8bit(80);
+        }else if(ADCH < (base - band)){
+            //turning tword the wall
+            PORTD |= (1<<PD1);
+            OCR0A = percent_to_8bit(80);
+            OCR2A = percent_to_8bit(30);
+        }else {
+            //go forward
+            PORTD |= (1<<PD1)|(1<<PD3);
+            OCR0A = percent_to_8bit(60);
+            OCR2A = percent_to_8bit(60);
+        }
+        _delay_ms(500);   
     }
 }
