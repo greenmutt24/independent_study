032365d078c8d33e17ea5ecfdb48ec85a0cce8a0
Anthony Webb <ajwebb01@gmail.com>
2012-05-01 09:49:54 -0400

    sonar files start

---
+++ sonar.c
@@ -0,0 +1,15 @@
+/**********************************************************
+*Anthony Webb
+*AVR parralx Ping))) sonar .c file
+**********************************************************/
+
+#include "sonar.h"
+
+int get_time(){
+
+}
+
+int get_distance_cm(){
+
+}
+
--- sonar.h
+++ sonar.h
@@ -1,19 +1,13 @@
 /**********************************************************
 *Anthony Webb
-*AVR USART .h file
+*AVR parralx Ping))) sonar .h file
 **********************************************************/
 
 #include "cpu_clock.h"
 #include <avr/io.h>
 #include <util/delay.h>
-#include <string.h>
 
-#define BAUD_RATE 4800
-#define UBBR_VALUE ((F_CPU / (BAUD_RATE * 16UL)) - 1)
+int get_time();
 
-void usart_init();
-
-char usart_read();
-
-void usart_write(char data);
+int get_distance_cm();
 
